bd4c30480006729a8609181800f275fa09642918
Ezekiel Kigbo <theatlasroom@gmail.com>
2016-03-22 12:25:03 +1100

    Interface cleanup
    - cleaned up the staff table
    - cleaned up the filter tab
    
    added basic filter for the documents - connected the selected tab
    
    moved teams, categories and topics to seperate services

--- public/js/controllers/documents-index.controller.js
+++ public/js/controllers/documents-index.controller.js
@@ -13,27 +13,8 @@
     $log.log(documentList);
 
     vm.activeTab = 'All documents';
-    /*vm.categories = [
-      {'label':'All documents', 'value':'', 'active': 'active'},
-      {'label':'Finance', 'value':'Finance', 'active':''},
-      {'label':'HR', 'value':'HR','active':''},
-      {'label':'OH&S', 'value':'OH&S','active':''}
-    ];*/
-
     vm.categories = Categories.data;
     vm.topics = Topics.data;
-
-    /*// move this to a service
-    vm.topics = [
-      {topic: "Client feedback", category: "HR"},
-      {topic: "Leave", category: "HR"},
-      {topic: "Workplace functions", category: "HR"},
-      {topic: "Aggresive clients", category: "OH&S"},
-      {topic: "Chemicals", category: "OH&S"},
-      {topic: "Emergencies", category: "OH&S"},
-      {topic: "First aid", category: "OH&S"},
-      {topic: "Risk assessment", category: "OH&S"},
-    ];*/
 
     function init(){
       $log.log("Loaded the documents index controller");
