8fad75e3cdc26d06e10a018ecac9b80d4366413f
Ezekiel Kigbo <theatlasroom@gmail.com>
2016-05-02 19:36:48 +1000

    Basic webpack configuration - preparing modules to use commonjs + webpack

---
+++ public/index.js
@@ -0,0 +1,5 @@
+// create our module
+var angular = require('angular');
+
+var app = angular.module('njcIntranetApp', []);
+console.log(app);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+console.log(__dirname);
+
+module.exports = {
+  context: __dirname + '/public',
+  entry: ['./index.js'],
+  output: {
+    path: __dirname + '/public/build',
+    publicPath: __dirname + '/public/js',
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    contentBase: 'public'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.es6'] // not sure what this does
+  }
+};
